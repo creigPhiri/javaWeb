aa76338cef786bc32a5ac86277b0a5bef8ae6dda
Creig Fortune <fortunephiri004@gmail.com>
2018-10-24 08:31:09 -0400

    created the dashboard.html, registration.html, login.html. these files are used for testing. both auth files call the same servlet. which will handle the routing. at this point when the servlet is called it returns a page stating where the request originated

---
+++ Assignment1/src/auth/servlets/Authentication.java
@@ -0,0 +1,50 @@
+package auth.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Authentication
+ */
+@WebServlet("/Authentication")
+public class Authentication extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Authentication() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		if(request.getParameter("authType").contains("login")) {
+			pw.println("this was requested by login page");
+		}else if(request.getParameter("authType").contains("registration")) {
+			pw.println("this was requested by the registration page");
+			}
+	
+//		response.sendRedirect(request.getContextPath()+"/Dashboard.html");
+	}
+
+}
